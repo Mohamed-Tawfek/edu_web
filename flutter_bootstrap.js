351fd642cd93ddfd74f521ae0612fd215b7f124a
Mohamed Tawfek <mohamed.tawfek.eng@gmail.com>
2024-09-10 15:58:54 +0300

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "397960938"
+    serviceWorkerVersion: "3396980833"
   }
 });
